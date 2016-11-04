f77e037f59331c4f17985a147e42f4af635d4cdf
talbor49 <talbor49@gmail.com>
2016-11-04 20:50:54 +0200

    Made connection functions extension methods

--- client.go
+++ client.go
@@ -1,19 +1,33 @@
 package client
 
 import (
+	"bufio"
 	"fmt"
 	"net"
-	"bufio"
 )
 
-var conn net.Conn
+type dbConn struct {
+	net.Conn
+	Bucket   string
+	Username string
+}
 
-func Set(key string, value string) {
+//Connect creates a TCP database connection with the specified server
+func Connect(ip string, port int) dbConn {
+	addr := fmt.Sprintf("%s:%d", ip, port)
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		panic(err.Error())
+	}
+	return dbConn{conn, "", ""}
+}
+
+func (conn *dbConn) Set(key string, value string) {
 	request := fmt.Sprintf("SET %s %s", key, value)
 	fmt.Fprintf(conn, request)
 }
 
-func Get(key string) string {
+func (conn *dbConn) Get(key string) string {
 	request := fmt.Sprintf("GET %s", key)
 	fmt.Fprintf(conn, request)
 	message, err := bufio.NewReader(conn).ReadString('\n')
@@ -24,31 +38,21 @@ func Get(key string) string {
 	return message
 }
 
-func DeleteKey(key string) {
+func (conn *dbConn) DeleteKey(key string) {
 	request := fmt.Sprintf("DELETE KEY %s", key)
 	fmt.Fprintf(conn, request)
 }
 
-func DeleteBucket(bucket string) {
+func (conn *dbConn) DeleteBucket(bucket string) {
 	request := fmt.Sprintf("DELETE BUCKET %s", bucket)
 	fmt.Fprintf(conn, request)
 }
 
-func Authenticate(user string, password string, bucket string) {
+func (conn *dbConn) Authenticate(user string, password string, bucket string) {
 	request := fmt.Sprintf("AUTH %s %s %s", user, password, bucket)
 	fmt.Fprintf(conn, request)
 }
 
-func Connect(ip string, port int) {
-	addr := fmt.Sprintf("%s:%d", ip, port)
-	_conn, err := net.Dial("tcp", addr)
-	conn = _conn
-	if err != nil {
-		panic(err.Error())
-	}
-}
-
-
-func Disconnect() {
+func (conn *dbConn) Disconnect() {
 	conn.Close()
 }
