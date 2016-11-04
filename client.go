cdc4169feee37fdb2805bcb630978369496c3f10
talbor49 <talbor49@gmail.com>
2016-11-04 21:18:17 +0200

    Replaced fmt.FPrintF to conn.Write to avoid confusion between writes in server

--- client.go
+++ client.go
@@ -24,12 +24,12 @@ func Connect(ip string, port int) dbConn {
 
 func (conn *dbConn) Set(key string, value string, options string) {
 	request := fmt.Sprintf("SET %s %s %s", key, value, options)
-	fmt.Fprintf(conn, request)
+	conn.Write([]byte(request))
 }
 
 func (conn *dbConn) Get(key string, options string) string {
 	request := fmt.Sprintf("GET %s %s", key, options)
-	fmt.Fprintf(conn, request)
+	conn.Write([]byte(request))
 	message, err := bufio.NewReader(conn).ReadString('\n')
 	if err != nil {
 		//just for now
@@ -40,18 +40,18 @@ func (conn *dbConn) Get(key string, options string) string {
 
 func (conn *dbConn) DeleteKey(key string, options string) {
 	request := fmt.Sprintf("DELETE KEY %s %s", key, options)
-	fmt.Fprintf(conn, request)
+	conn.Write([]byte(request))
 }
 
 func (conn *dbConn) DeleteBucket(bucket string, options string) {
 	request := fmt.Sprintf("DELETE BUCKET %s %s", bucket, options)
-	fmt.Fprintf(conn, request)
+	conn.Write([]byte(request))
 }
 
 func (conn *dbConn) Authenticate(user string, password string, bucket string) {
 	request := fmt.Sprintf("AUTH %s %s %s", user, password, bucket)
 	fmt.Printf(request)
-	fmt.Fprintf(conn, request)
+	conn.Write([]byte(request))
 }
 
 func (conn *dbConn) Disconnect() {
